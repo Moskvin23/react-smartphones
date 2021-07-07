518479b8a920d2c23197b51e01246cfa45e94a8f
Pasha <23moskvin@gmail.com>
2021-07-07 10:39:08 +0300

    I've added cart, input for my site
    Also I made boot states when downloading page( made for a convenience) To work with a layout , set (or installed a sass package)

--- react-phones/src/App.js
+++ react-phones/src/App.js
@@ -2,7 +2,60 @@
 function App() {
   return (
   <div className="wrapper clear"> 
-  <header className="d-flex justify-between align-center">
+  <div className="cart-basket-shadow">
+    <div className="cart-basket">
+      <h2 className="d-flex justify-between mb-30">Корзина<img className="removeBtn cu=p" src="/img/remove-btn.svg" alt="Remove"/></h2>
+
+      <div className="items">
+      <div className="cartItem d-flex align-center">
+        <img className="mr-5" width={130} height={130} src="/img/smartphones/Apple iPhone 11 Pro 256GB Space Gray.jpg" alt="Smartphone"/>
+      <div>
+        <p className="mb-5">Apple iPhone 11 Pro 256GB Space Gray</p>
+        <b>Цена 15 898 грн.</b>
+      </div>
+      <img className="removeBtn" src="/img/remove-btn.svg" alt="Remove"/>
+      </div>
+
+      <div className="cartItem d-flex align-center">
+        <img className="mr-5" width={130} height={130} src="/img/smartphones/Apple iPhone 11 Pro 256GB Space Gray.jpg" alt="Smartphone"/>
+      <div>
+        <p className="mb-5">Apple iPhone 11 Pro 256GB Space Gray</p>
+        <b>Цена 15 898 грн.</b>
+      </div>
+      <img className="removeBtn" src="/img/remove-btn.svg" alt="Remove"/>
+      </div>
+      <div className="cartItem d-flex align-center">
+        <img className="mr-5" width={130} height={130} src="/img/smartphones/Apple iPhone 11 Pro 256GB Space Gray.jpg" alt="Smartphone"/>
+      <div>
+        <p className="mb-5">Apple iPhone 11 Pro 256GB Space Gray</p>
+        <b>Цена 15 898 грн.</b>
+      </div>
+      <img className="removeBtn" src="/img/remove-btn.svg" alt="Remove"/>
+      </div>
+      </div>
+
+      <div className="cartTotalBlock">
+      <ul>
+        <li>
+          <span>Итого:</span>
+          <div></div>
+          <b>31 796 грн.</b>
+        </li>
+        <li>
+        <span>Налог 5%:</span>
+          <div></div>
+          <b>1589.8 грн.</b>
+        </li>
+      </ul>
+      <button>Оформить заказ</button>
+      </div>
+      
+    </div>
+  </div>
+
+
+
+  <header className="d-flex justify-between align-center p-40">
     <div className="d-flex align-center">
     <img width={60} height={60} src="/logo23.jpg" /> 
     <div>
@@ -11,15 +64,24 @@ function App() {
     </div>
     </div>
     <ul className="d-flex">
-      <li className="mr-30"><img width={25} height={25} src="/img/cart.svg"/><span>12 565 грн.</span></li>
-      <li><img width={18} height={18} src="/img/user.svg"/></li>
+      <li className="mr-30"><img width={20} height={20} src="/img/cart1.svg"/><span> 12 565 грн.</span></li>
+      <li><img width={20} height={20} src="/img/user1.svg"/></li>
     </ul>
-  </header>
+  </header> 
     <div className="content p-40">
-      <h1 className="mb-40">Все телефоны </h1>
+      <div className="d-flex align-center justify-between mb-40">
+        <h1>Все телефоны </h1>
+        <div className="search-block d-flex">
+          <img src="/img/search.svg" alt="Search" />
+          <input placeholder="Поиск...."/>
+        </div>
+        </div>
 
       <div className="smartphones1 d-flex">
       <div className="card">
+       <div className="favorite">
+       <img src="/img/bookmark-liked.svg" alt="Liked"/>
+       </div>
         <img width={130} height={120} src="/img/smartphones/Apple iphone 11 Pro 256GB Space Gray.jpg" alt="Smartphones"/>
         <h5>Apple iPhone 11 Pro 256GB Space Gray</h5>
         <div className="d-flex justify-between align-center">
